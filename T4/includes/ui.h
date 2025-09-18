58f315131dc959ed41b8b4a7057d91c410eb63ab
matncb <matheus.brustelo@gmail.com>
2025-09-17 23:20:24 -0300

    consts for compiler

--- T4/includes/ui.h
+++ T4/includes/ui.h
@@ -5,6 +5,10 @@
 #define MAX_CMD 5
 #define FULL_CMD_LENGTH MAX_CMD_LENGTH*MAX_CMD+MAX_CMD
 
+#define FORCE_RUN_CODES // Input/Saída completamente louca do run codes
+
+#ifndef FORCE_RUN_CODES
+
 #ifdef WIN32
 #define END_LINE "\r\n"
 #endif
@@ -12,6 +16,11 @@
 #ifdef __linux__
 #define END_LINE "\n"
 #endif
+
+#else
+#define END_LINE "\r\n"
+#endif
+
 
 void ui_run();
 
